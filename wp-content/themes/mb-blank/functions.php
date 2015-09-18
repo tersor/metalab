3abc11d787119853ee9d8b7774b01cf2573ebf1c
Terje Sørbø <terje@mediebruket.no>
2015-09-18 16:00:35 +0200

    added feed css

--- wp-content/themes/mb-blank/functions.php
+++ wp-content/themes/mb-blank/functions.php
@@ -2,6 +2,7 @@
 
 function mblank_scripts() {
   wp_enqueue_style( 'styles', get_template_directory_uri() . '/style.css' );
+  wp_enqueue_style( 'feed_css', esc_url_raw( 'http://www.fotogalleriet.no/forms.css' ), array(), null );
 }
 add_action( 'wp_enqueue_scripts', 'mblank_scripts' );
 
