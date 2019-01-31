c4ea4a3aa539fb44dd0563d7583ca3ca8fa3218c
tersor <terje@mediebruket.no>
2019-01-31 09:31:28 +0100

    Oppdatert plugin: gravityforms

--- wp-content/plugins/gravityforms/includes/fields/class-gf-field-textarea.php
+++ wp-content/plugins/gravityforms/includes/fields/class-gf-field-textarea.php
@@ -58,7 +58,6 @@ class GF_Field_Textarea extends GF_Field {
 		$class         = esc_attr( $class );
 		$disabled_text = $is_form_editor ? 'disabled="disabled"' : '';
 
-		$logic_event           = $this->get_conditional_logic_event( 'keyup' );
 		$placeholder_attribute = $this->get_field_placeholder_attribute();
 		$required_attribute    = $this->isRequired ? 'aria-required="true"' : '';
 		$invalid_attribute     = $this->failed_validation ? 'aria-invalid="true"' : 'aria-invalid="false"';
@@ -70,7 +69,7 @@ class GF_Field_Textarea extends GF_Field {
 		}
 
 		//see if the field is set to use the rich text editor
-		if ( $this->useRichTextEditor && ! $is_admin ) {
+		if ( ! $is_admin && $this->is_rich_edit_enabled() ) {
 			//placeholders cannot be used with the rte; message displayed in admin when this occurs
 			//field cannot be used in conditional logic by another field; message displayed in admin and field removed from conditional logic drop down
 			$tabindex = GFCommon::$tab_index > 0 ? GFCommon::$tab_index ++ : '';
@@ -132,7 +131,7 @@ class GF_Field_Textarea extends GF_Field {
 				$input       = sprintf( '<div id="%s_rte_preview" class="gform-rte-preview %s" style="display:%s"></div>', $field_id, $size, $display );
 			}
 
-			$input .= "<textarea name='input_{$id}' id='{$field_id}' class='textarea {$class}' {$tabindex} {$logic_event} {$placeholder_attribute} {$required_attribute} {$invalid_attribute} {$disabled_text} {$input_style} rows='10' cols='50'>{$value}</textarea>";
+			$input .= "<textarea name='input_{$id}' id='{$field_id}' class='textarea {$class}' {$tabindex} {$placeholder_attribute} {$required_attribute} {$invalid_attribute} {$disabled_text} {$input_style} rows='10' cols='50'>{$value}</textarea>";
 
 		}
 
@@ -285,6 +284,58 @@ class GF_Field_Textarea extends GF_Field {
 
 		return $return;
 	}
+
+	/**
+	 * Determines if the RTE can be enabled for the current field and user.
+	 *
+	 * @since 2.2.5.14
+	 *
+	 * @return bool
+	 */
+	public function is_rich_edit_enabled() {
+		if ( ! $this->useRichTextEditor ) {
+			return false;
+		}
+
+		global $wp_rich_edit;
+		$wp_rich_edit = null;
+
+		add_filter( 'get_user_option_rich_editing', array( $this, 'filter_user_option_rich_editing' ) );
+		$user_can_rich_edit = user_can_richedit();
+		remove_filter( 'get_user_option_rich_editing', array( $this, 'filter_user_option_rich_editing' ) );
+
+		return $user_can_rich_edit;
+	}
+
+	/**
+	 * Filter the rich_editing option for the current user.
+	 *
+	 * @since 2.2.5.14
+	 *
+	 * @param string $value The value of the rich_editing option for the current user.
+	 *
+	 * @return string
+	 */
+	public function filter_user_option_rich_editing( $value ) {
+		return 'true';
+	}
+
+	// # FIELD FILTER UI HELPERS ---------------------------------------------------------------------------------------
+
+	/**
+	 * Returns the filter operators for the current field.
+	 *
+	 * @since 2.4
+	 *
+	 * @return array
+	 */
+	public function get_filter_operators() {
+		$operators   = parent::get_filter_operators();
+		$operators[] = 'contains';
+
+		return $operators;
+	}
+
 }
 
 GF_Fields::register( new GF_Field_Textarea() );
